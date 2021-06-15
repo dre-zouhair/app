929a741589f4ba91b3c0ecfbecd896dc9b61c3ae
dre-zouhair <dre.zouhair@gmail.com>
2021-06-16 00:01:04 +0200

    Adding Internship Model + migration

---
+++ database/migrations/2021_06_15_215731_create_internships_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInternshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('internships', function (Blueprint $table) {
+            $table->id();
+            $table->date("startDate");
+            $table->date("endDate");
+            $table->string("report");
+            $table->string("mark");
+            $table->unsignedBigInteger("enterprise_id");
+            $table->foreign("enterprise_id")->references("id")->on("enterprises");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('internships');
+    }
+}
