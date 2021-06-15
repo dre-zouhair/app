4de61e25e65a2f0baf9a42e6a8a08db893c9b421
dre-zouhair <dre.zouhair@gmail.com>
2021-06-15 23:52:53 +0200

    Adding Intern Model + filling migration its file

---
+++ database/migrations/2021_06_15_214814_create_interns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInternsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interns', function (Blueprint $table) {
+            $table->id();
+            $table->string("registrationNumber");//Matricule
+            $table->string("phone");
+            $table->string("placeOfBirth");
+            $table->date("dateOfBirth");
+            $table->unsignedBigInteger("user_id")->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interns');
+    }
+}
