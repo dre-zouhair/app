3a0e2ae6b001c1025f67f77c10f8c11a446974c8
dre-zouhair <dre.zouhair@gmail.com>
2021-06-17 01:08:57 +0200

    adding jetstream and livewire

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName');
+            $table->string('name');
             $table->string('lastName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum("user_type",["student","admin","enterpriseRep"])->default("student");
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
