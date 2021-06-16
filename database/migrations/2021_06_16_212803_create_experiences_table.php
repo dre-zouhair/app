b9873c890bb7860fc7dffad8df959bf7efc6d81f
dre-zouhair <dre.zouhair@gmail.com>
2021-06-16 23:30:27 +0200

    Adding Experience Model + migration

---
+++ database/migrations/2021_06_16_212803_create_experiences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string("enterprise");
+            $table->string("desc");
+            $table->date("startDate");
+            $table->date("endDate");
+            $table->unsignedBigInteger("intern_id");
+            $table->foreign("intern_id")->references("id")->on("interns");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
