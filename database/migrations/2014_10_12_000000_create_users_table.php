d78cc18f31fc30b5f28b9e05755ea69d28a01165
dre-zouhair <dre.zouhair@gmail.com>
2021-06-17 20:25:57 +0200

    Creating middlewares

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum("user_type",["student","admin","enterpriseRep"])->default("student");
+            $table->enum("user_type",["intern","admin","enterpriseRep"])->default("intern");
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
