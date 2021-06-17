5ef4a329325ef39f7e5c4cdb1c5a55bd8d65bb57
dre-zouhair <dre.zouhair@gmail.com>
2021-06-17 17:15:28 +0200

    restoring Convention

---
+++ database/migrations/2021_06_17_151428_create_conventions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConventionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conventions', function (Blueprint $table) {
+            $table->id();
+            $table->date("startDate");
+            $table->date("endDate");
+            $table->unsignedBigInteger("submission_id");
+            $table->foreign('submission_id')->references('id')->on('submissions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conventions');
+    }
+}
