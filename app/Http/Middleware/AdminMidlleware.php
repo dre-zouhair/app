95a4bf21e773882c473ce6e18011174dc919cc94
dre-zouhair <dre.zouhair@gmail.com>
2021-06-17 20:45:08 +0200

    Adding redirect logic

--- app/Http/Middleware/AdminMidlleware.php
+++ app/Http/Middleware/AdminMidlleware.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMidlleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if(User::all()->find(Auth::user()->getAuthIdentifier())->user_type  == "admin"){
+            return $next($request);
+        }
+        return redirect()->back();
     }
 }
--- app/Http/Middleware/EnterpriseMidlleware.php
+++ app/Http/Middleware/EnterpriseMidlleware.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EnterpriseMidlleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if(User::all()->find(Auth::user()->getAuthIdentifier())->user_type == "enterpriseRep"){
+            return $next($request);
+        }
+        return redirect()->back();
     }
 }
