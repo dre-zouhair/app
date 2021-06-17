bb28b66245a8c4568e3a5d5b976bad88b292d054
dre-zouhair <dre.zouhair@gmail.com>
2021-06-17 20:29:03 +0200

    assigning a middleware key in app/Http/Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'intern' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'enterprise' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
