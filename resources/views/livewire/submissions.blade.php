78428ed670e29f9fb2ed10ff95f560bc79e8273e
dre-zouhair <dre.zouhair@gmail.com>
2021-06-26 18:03:56 +0200

    Adding logs

--- resources/views/livewire/submissions.blade.php
+++ resources/views/livewire/submissions.blade.php
@@ -1,4 +1,32 @@
 <div class="p-8 mb-8 grid grid-cols-1">
+    <div class="grid grid-rows-1">
+        @if (session()->has('message'))
+            <div class="text-white px-6 py-4 border-0  relative mb-4 bg-green-500">
+                    <span class="text-xl inline-block mr-5 align-middle">
+                    <i class="fas fa-bell"></i>
+                </span>
+                <span class="inline-block align-middle mr-8">
+                    {{ session('message') }}
+                </span>
+                <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="closeAlert(event)">
+                    <span>×</span>
+                </button>
+            </div>
+        @endif
+        @if (session()->has('error'))
+            <div class="text-white px-6 py-4 border-0  relative mb-4 bg-red-500">
+                    <span class="text-xl inline-block mr-5 align-middle">
+                    <i class="fas fa-bell"></i>
+                </span>
+                <span class="inline-block align-middle mr-8">
+                    {{ session('error') }}
+                </span>
+                <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="closeAlert(event)">
+                    <span>×</span>
+                </button>
+            </div>
+        @endif
+    </div>
     <div class="bg-white shadow-lg mx-2 p-2 col-span-1 border-b border-gray-200">
 
         @foreach($submissions as $value)
@@ -16,6 +44,3 @@
             @endif
     </div>
 </div>
-
-
-
