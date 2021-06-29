1a3164bc18952c538d358b733031e38cbcb8965d
dre-zouhair <dre.zouhair@gmail.com>
2021-06-30 01:50:13 +0200

    adding message

--- resources/views/livewire/list-internships.blade.php
+++ resources/views/livewire/list-internships.blade.php
@@ -1,32 +1,37 @@
  <div class="p-8 mb-8 grid grid-cols-3">
         <div class="bg-white shadow-lg mx-2 p-2 col-span-1 border-b border-gray-200">
-            @foreach($internships as $value)
-                <div class="box-border border-2 2xl:border-blue-100 p-2 mt-2" wire:click="internship({{$value['id']}})">
-                    <h2 class="text-2xl mb-2">{{$value['title']}} </h2>
-                    <h1 class="text-blue-300 text-xl" > Submissions :{{\App\Models\Internship::find($value['id'])->submissions()->getResults()->count()}} </h1>
-                    <p> Starts at : {{$value['startDate']}}</p>
-                    <p> Duration : {{$value['duration']}}</p>
-                    <p> This offer expires at : {{$value['expirationDate']}}</p>
-                </div>
-            @endforeach
-
-            @if($index> 0)
-                @if(! sizeof($internships))
-                    <div class="text-center">
-                        @endif
-                        <button class="mt-8 bg-transparent hover:bg-yellow-400 text-yellow-600 font-semibold hover:text-white py-2 px-4 border border-yellow-400 hover:border-transparent m-2 w-4/12" wire:click="paginate(-1)">
-                            Back
-                        </button>
-                        @if(! sizeof($internships))
+            @if(sizeof($internships))
+                @foreach($internships as $value)
+                    <div class="box-border border-2 2xl:border-blue-100 p-2 mt-2" wire:click="internship({{$value['id']}})">
+                        <h2 class="text-2xl mb-2">{{$value['title']}} </h2>
+                        <h1 class="text-blue-300 text-xl" > Submissions :{{\App\Models\Internship::find($value['id'])->submissions()->getResults()->count()}} </h1>
+                        <p> Starts at : {{$value['startDate']}}</p>
+                        <p> Duration : {{$value['duration']}}</p>
+                        <p> This offer expires at : {{$value['expirationDate']}}</p>
                     </div>
-                @endif
-            @endif
-            @if(sizeof($internshipsList) > 5 && $index < sizeof($internshipsList) )
-                <button class="mt-8 bg-transparent hover:bg-green-400 text-green-600 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent m-2 w-4/12" wire:click="paginate(1)">
-                    Next
-                </button>
-            @endif
+                @endforeach
 
+                @if($index> 0)
+                    @if(! sizeof($internships))
+                        <div class="text-center">
+                            @endif
+                            <button class="mt-8 bg-transparent hover:bg-yellow-400 text-yellow-600 font-semibold hover:text-white py-2 px-4 border border-yellow-400 hover:border-transparent m-2 w-4/12" wire:click="paginate(-1)">
+                                Back
+                            </button>
+                            @if(! sizeof($internships))
+                        </div>
+                    @endif
+                @endif
+                @if(sizeof($internshipsList) > 5 && $index < sizeof($internshipsList) )
+                    <button class="mt-8 bg-transparent hover:bg-green-400 text-green-600 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent m-2 w-4/12" wire:click="paginate(1)">
+                        Next
+                    </button>
+                @endif
+            @else
+                <div class="box-border border-2 2xl:border-blue-100 p-2 mt-2">
+                Try adding some offers !
+                </div>
+            @endif
         </div>
 
         @if($display)
