a49e61d28270738b0d148d901a11279b6368f9fa
dre-zouhair <dre.zouhair@gmail.com>
2021-06-30 01:44:32 +0200

    adding danger color

--- resources/views/livewire/list-submissions.blade.php
+++ resources/views/livewire/list-submissions.blade.php
@@ -149,7 +149,7 @@
                     <button class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent m-2 w-4/12" wire:click.prevent="accept()">
                         Accept </button>
 
-                    <button class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent m-2 w-4/12" wire:click.prevent="reject()">
+                    <button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent m-2 w-4/12" wire:click.prevent="reject()">
                         Reject </button>
                 </div>
 
